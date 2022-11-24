6cc431119871ea2ed43c4ca5ac908700fb7142fb
Gonzalo <gonzalo.hernandez1@hotmail.com>
2022-11-24 15:55:09 -0300

    getContactById

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -8,6 +8,8 @@ import {
   Query,
   ParseIntPipe,
   DefaultValuePipe,
+  Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { Contact } from './contact.schema';
 import { ContactsService } from './contacts.service';
@@ -30,5 +32,12 @@ export class ContactsController {
     @Body() body?: Contact,
   ) {
     return this.service.getAllContacts(_limit, _page, body.email);
+  }
+
+  @Get('/:id')
+  async getContactById(@Param('id') id: string) {
+    const chosenContact = await this.service.getContactById(id);
+    if (!chosenContact) throw new NotFoundException();
+    return chosenContact;
   }
 }
