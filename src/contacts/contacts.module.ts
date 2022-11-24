9e272398b4c64c35aec16888210de349bc5ec2c1
Gonzalo <gonzalo.hernandez1@hotmail.com>
2022-11-24 14:21:55 -0300

    getAllComponentsRouteAndDebug

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ContactSchema } from './contact.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Contact', schema: ContactSchema }]),
+  ],
   controllers: [ContactsController],
-  providers: [ContactsService]
+  providers: [ContactsService],
 })
 export class ContactsModule {}
